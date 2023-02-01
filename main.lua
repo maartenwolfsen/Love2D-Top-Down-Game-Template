5a764e235e52b699fae1ad836d5f9cec3bcd81f3
Maarten Wolfsen <maartenwolfsen@gmail.com>
2023-02-01 08:41:15 +0100

    Improve player rendering

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 ﻿local sti = require "lib/sti"
 local mapPos = { x = 0, y = 0 }
-local mapSpeed = 4
+local speed = 2
 local mapScale = 4
 local window = {
   w = love.graphics.getWidth(),
@@ -21,40 +21,44 @@ function love.load()
 	spriteLayer.sprites = {
 		player = {
 			image = playerImage,
-			x = 16,
-			y = 16,
+			x = 0,
+			y = 0,
 			r = 0,
-      w = playerImage:getWidth(),
-      h = playerImage:getHeight()
+      w = playerImage:getWidth() / (mapScale ^ 2),
+      h = playerImage:getHeight() / (mapScale ^ 2)
 		}
 	}
   
 	function spriteLayer:update(dt)
 		for _, sprite in pairs(self.sprites) do
       if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
-        sprite.y = sprite.y - 1
+        sprite.y = sprite.y - speed
       end
 
       if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
-        sprite.y = sprite.y + 1
+        sprite.y = sprite.y + speed
       end
 
       if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
-        sprite.x = sprite.x - 1
+        sprite.x = sprite.x - speed
       end
 
       if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
-        sprite.x = sprite.x + 1
+        sprite.x = sprite.x + speed
       end
 		end
 	end
   
 	function spriteLayer:draw()
 		for _, sprite in pairs(self.sprites) do
-			local x = math.floor(sprite.x)
-			local y = math.floor(sprite.y)
-			local r = sprite.r
-			love.graphics.draw(sprite.image, x, y, r)
+			love.graphics.draw(
+        sprite.image,         -- Sprite
+        math.floor(sprite.x), -- X
+        math.floor(sprite.y), -- Y
+        sprite.r,             -- Rotation
+        sprite.w,             -- Width
+        sprite.h              -- Height
+      )
 		end
 	end
 end
@@ -66,20 +70,9 @@ end
 function love.draw()
   playerSprite = spriteLayer.sprites.player
   
-	map:draw(
-    -playerSprite.x + ((window.w / mapScale / 2) - (playerSprite.w / 2)),
-    -playerSprite.y + ((window.h / mapScale / 2) - (playerSprite.h / 2)),
+  map:draw(
+    -playerSprite.x + ((window.w / mapScale / 2) - (playerSprite.w * (mapScale * 2))),
+    -playerSprite.y + ((window.h / mapScale / 2) - (playerSprite.h * (mapScale * 2))),
     mapScale
-  )
-  
-  love.graphics.draw(
-    player.sprite,
-    math.floor(player.x),
-    math.floor(player.y),
-    0,
-    1,
-    1,
-    player.sprite:getWidth(),
-    player.sprite:getHeight()
   )
 end
