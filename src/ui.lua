59d44ce11f6d0ffb9cdbf1278bf33a8137ae64d0
Maarten Wolfsen <maartenwolfsen@gmail.com>
2023-02-21 08:00:34 +0100

    Add custom font

--- src/ui.lua
+++ src/ui.lua
@@ -1,7 +1,8 @@
 require "src/core/window"
 
 Ui = {
-	font = love.graphics.getFont()
+	font = love.graphics.setNewFont("font/Symtext.ttf", 16),
+	deathText = "Oh Dear, You're Dead!"
 }
 
 Ui.draw = function()
@@ -15,7 +16,17 @@ Ui.drawDeathScreen = function()
 	love.graphics.setColor(0,0,0,0.5)
 	love.graphics.rectangle("fill", 0, 0, Window.w, Window.h)
 	love.graphics.setColor(255,255,255)
-  	love.graphics.printf("Oh Dear, You're Dead!", 0, Window.h / 2 - Ui.font:getHeight() / 2, Window.w, "center")
+	love.graphics.setFont(Ui.font)
+  	love.graphics.print(
+  		Ui.deathText,
+  		Window.w / 2,
+  		Window.h / 2,
+  		0,
+  		1,
+  		1,
+  		Ui.font:getWidth(Ui.deathText) / 2,
+  		Ui.font:getHeight() / 2
+	)
 end
 
 return Ui
